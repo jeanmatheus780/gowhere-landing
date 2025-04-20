60306bdcf1104b7e2ee0a93c5a0fb5fbcdad0fb9
Jean Matheus <jeanbta_01@hotmail.com>
2025-04-19 22:07:22 -0300

    Adicionar configuração para habilitar a estrutura 'app'

---
+++ next.config.js/next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: true, // Ativa o modo estrito do React
+    experimental: {
+      appDir: true, // Ativa a estrutura do app (novos recursos do Next.js 13)
+    },
+  }
+  
+  module.exports = nextConfig;
+  
